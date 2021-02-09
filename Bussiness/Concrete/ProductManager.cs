2742d48913df19f77a1c4cd2bbd7b3e6c3e4c5b4
EMRE <emreciicek@gmail.com>
2021-02-09 14:36:40 +0300

    Proje dosyası ekle.

---
+++ Bussiness/Concrete/ProductManager.cs
@@ -0,0 +1,22 @@
+﻿using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bussiness.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAll()
+        {
+            return _productDal.GetAll();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Bussiness.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var item in productManager.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
